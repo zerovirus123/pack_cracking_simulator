888f2f89beac9cc27c020da3227822eaf65d2f5c
zerovirus123 <briansia93@gmail.com>
2021-07-04 12:01:59 +0700

    refactored flip card logic

--- app/static/index.js
+++ app/static/index.js
@@ -1,15 +1,22 @@
-function flip(uri_dict)
+function flip_card()
 {
-	console.log("called card flip")
+	console.log("called card flip");
 	let mdfc = document.getElementById("mdfc");
 	let src = mdfc.src;
 
+	console.log(src);
+	console.log(uri_dict);
+	console.log(uri_dict["front_uri"]);
+	console.log(uri_dict["back_uri"])
+
 	if (src == uri_dict["front_uri"])
 	{
+		console.log("flip to back");
 		mdfc.src = uri_dict["back_uri"]
 	}
 	else
 	{
+		console.log("flip to front");
 		mdfc.src = uri_dict["front_uri"]
 	}
 }
