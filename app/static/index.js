48765db5b49b77370f4338c1574a48ba3a9af973
zerovirus123 <briansia93@gmail.com>
2021-07-06 15:05:20 +0700

    added request caching for set icons

--- app/static/index.js
+++ app/static/index.js
@@ -1,3 +1,5 @@
+let cache = {};
+
 function flip_card()
 {
 	let mdfc = document.getElementById("mdfc");
@@ -17,28 +19,35 @@ function flip_card()
 
 function select_set()
 {
-	let select = document.getElementsByName("set-select");
+	const select = document.getElementsByName("set-select");
 	let generate_button = document.getElementById("generate-button");
 	let set_icon = document.getElementById("set-icon");
-
-	const URI = "https://api.scryfall.com//sets//" + select[0].value;
-	const Http = new XMLHttpRequest();
-
-	Http.open("GET", URI);
-	Http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-	Http.send();
-
-	Http.onreadystatechange = (e) => {
-		json = JSON.parse(Http.responseText);
-		set_icon.src = json["icon_svg_uri"];
-	}
-
-	if (select[0].value == "Select a Set")
+	let set_code = select[0].value;
+	
+	if (set_code == "Select a Set")
 	{
 		generate_button.disabled = true;
 	}
 	else
 	{
+		if (cache[set_code]){
+			set_icon.src = cache[set_code];
+		}
+		else
+		{
+			const URI = "https://api.scryfall.com//sets//" + set_code;
+			const Http = new XMLHttpRequest();
+	
+			Http.open("GET", URI);
+			Http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+			Http.send();
+	
+			Http.onreadystatechange = (e) => {
+				json = JSON.parse(Http.responseText);
+				set_icon.src = json["icon_svg_uri"];
+				cache[set_code] = json["icon_svg_uri"]
+			}
+		}
 		generate_button.disabled = false;
 	}
 }
