6dbba1d3bc063f4fa2112e1904143ae0266877ab
zerovirus123 <briansia93@gmail.com>
2021-07-06 11:12:50 +0700

    fixed the MDFC card flip logic

--- app/static/index.js
+++ app/static/index.js
@@ -1,25 +1,16 @@
 function flip_card()
 {
-	console.log("called card flip");
 	let mdfc = document.getElementById("mdfc");
 	let src = mdfc.src;
-	let uri_dict = {"front_uri": mdfc.dataset.data_front, "back_uri": mdfc.dataset.data_back};
-
-	console.log("Img src: " + src);
-	console.log("Card dict: " + uri_dict);
-	console.log(mdfc.dataset.data_front);
-	console.log(mdfc.dataset.data_back);
-	console.log(uri_dict["front_uri"]);
-	console.log(uri_dict["back_uri"])
+	let uri_dict = {"front_uri": mdfc.getAttribute("data-front"), 
+	                "back_uri": mdfc.getAttribute("data-back")};
 
 	if (src == uri_dict["front_uri"])
 	{
-		console.log("flip to back");
 		mdfc.src = uri_dict["back_uri"]
 	}
 	else
 	{
-		console.log("flip to front");
 		mdfc.src = uri_dict["front_uri"]
 	}
 }
