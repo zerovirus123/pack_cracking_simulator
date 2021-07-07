7a3d5741f9ef43e7afaa83e6da45f2e6af57a5a9
zerovirus123 <briansia93@gmail.com>
2021-07-07 18:11:32 +0700

    added 404 handlers. added sets to ignore when searching for sets

--- app/home/views.py
+++ app/home/views.py
@@ -3,6 +3,7 @@ import requests_cache
 import time
 from .generate_pack import PackGenerator
 from datetime import timedelta
+from urllib.error import HTTPError
 
 views = Flask(__name__)
 views.debug = True
@@ -10,20 +11,27 @@ views.debug = True
 home_blueprint = Blueprint('home', __name__)
 
 sets = []
+sets_to_ignore = ["fmb1"]
 scryfall_API_base = "https://api.scryfall.com"
 
 @home_blueprint.route('/', methods=['GET'])
 def dropdown():
-	global sets
+	global sets, sets_to_ignore
 	request_session = requests_cache.CachedSession('session_cache', expire_after=timedelta(days=1))
 	req = scryfall_API_base + "/sets/"
 	time.sleep(5/1000)
-	response = request_session.get(req)
+	try:
+		response = request_session.get(req)
+	except HTTPError as err:
+		if err.code == 404:
+			print("404: {} is not a valid URI".format(req))
+
 	set_json = response.json()
 
 	for set in set_json["data"]:
-		if set["set_type"] == "expansion" or set["set_type"] == "draft_innovation" or set["set_type"] == "masters":
-			sets.append(set["code"])
+		if set["code"].lower() not in sets_to_ignore:
+			if set["set_type"] == "expansion" or set["set_type"] == "draft_innovation" or set["set_type"] == "masters":
+				sets.append(set["code"])
 
 	return render_template('home.html', sets=sets)
 
