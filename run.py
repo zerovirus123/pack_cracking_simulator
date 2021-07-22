8ebc016f2e67b9a6c2b13384a61a24598b37c4ef
zerovirus123 <briansia93@gmail.com>
2021-07-22 17:32:21 +0700

    added waitress for deploying project to production

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
 from app import create_app
-
+from waitress import serve
+from urllib.error import HTTPError
+import concurrent.futures
+import threading
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+	try:
+		host = '0.0.0.0'
+		port = 8080
+		print("Running application server in IP address {}, port {}...".format(host, port))
+		serve(app, host=host, port=port)
+	except HTTPError as err:
+		print(err)
