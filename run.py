ec6ce8ab4b5c9497886ddfa48a6d8082128122b3
zerovirus123 <briansia93@gmail.com>
2024-09-28 12:28:51 +0700

    CHORE: add formatting

--- run.py
+++ run.py
@@ -1,16 +1,19 @@
 from app import create_app
 from waitress import serve
 from urllib.error import HTTPError
-import concurrent.futures
-import threading
+
+# import concurrent.futures
+# import threading
 
 app = create_app()
 
 if __name__ == "__main__":
-	try:
-		host = '0.0.0.0'
-		port = 8080
-		print("Running application server in IP address {}, port {}...".format(host, port))
-		serve(app, host=host, port=port)
-	except HTTPError as err:
-		print(err)
+    try:
+        host = "0.0.0.0"
+        port = 8080
+        print(
+            "Running application server in IP address {}, port {}...".format(host, port)
+        )
+        serve(app, host=host, port=port)
+    except HTTPError as err:
+        print(err)
